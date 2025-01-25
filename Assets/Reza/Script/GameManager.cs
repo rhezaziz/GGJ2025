48696afb2bfdd1df4d6100d926a2a87278820180
unknown <rheza.khairullahaziz10@gmail.com>
2025-01-25 18:37:18 +0700

    Spawn

--- Assets/Reza/Script/GameManager.cs
+++ Assets/Reza/Script/GameManager.cs
@@ -7,8 +7,42 @@ public class GameManager : MonoBehaviour
     [System.Serializable]
     public class DataObj{
         public float timer;
-        public float speed;
-        public List<Barang> barangs;
-        public List<GameObject> obstacle; 
+        public List<Spawner> spawners;
+    }
+
+
+    public List<DataObj> objS;
+    public int level;
+    float intervalTime;
+    float nextTime;
+
+    void Start(){
+        initLevel();
+        
+        
+    }
+
+    void Update(){
+        
+        if(Time.time >= nextTime){
+            int num1 = Random.Range(0,objS[level].spawners.Count-1);
+            int num2 = Random.Range(0,objS[level].spawners.Count-1);
+
+            
+            
+
+            Instantiate(objS[level].spawners[num1],objS[level].spawners[num1].transform.position,objS[level].spawners[num1].transform.rotation);
+            objS[level].spawners[num1].getListBarang();
+            Instantiate(objS[level].spawners[num2],objS[level].spawners[num2].transform.position,objS[level].spawners[num2].transform.rotation);
+            objS[level].spawners[num2].getListBarang();
+
+
+            nextTime = Time.time + intervalTime;
+        }
+    }
+
+    public void initLevel(){
+        intervalTime = objS[level].timer;
+
     }
 }
--- Assets/Reza/Script/Spawner.cs
+++ Assets/Reza/Script/Spawner.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<GameObject> barang;
+    public float speed = 10f; // Kecepatan peluru
+    public Vector2 direction = Vector2.right; // Arah default (horizontal ke kanan)
 
-    // Update is called once per frame
-    void Update()
-    {
+    
+    public void getListBarang(){
+        int num = Random.Range(0,barang.Count-1);
+        barang[num].gameObject.GetComponent<MoveBarang>().speed = this.speed;
+        barang[num].gameObject.GetComponent<MoveBarang>().direction= this.direction;
+
+        Instantiate(barang[num],this.transform.position,this.transform.rotation);
+
+        
         
     }
 }
